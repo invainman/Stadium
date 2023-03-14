f866fb9223815c82135a2bda730424e9fdb9abbc
Ramil Abilov <flowerramon@yandex.com>
2023-03-14 07:28:13 +0300

    Added my project

---
+++ src/test/java/socNet/Stadium/StadiumApplicationTests.java
@@ -0,0 +1,13 @@
+package socNet.Stadium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
